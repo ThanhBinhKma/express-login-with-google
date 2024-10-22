9999b658074d0f5ecdef9a1011de904d28a8e617
binhnt <binhnt@kaopiz.com>
2024-10-22 14:13:23 +0700

    Add jwt

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { authenticateWithGoogle, getHome,  googleRedirect} from "../controllers/authController";
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -12,4 +13,18 @@ router.get("/auth/google", authenticateWithGoogle);
 // Route nhận callback từ Google
 router.get("/auth/google/redirect", authenticateWithGoogle, googleRedirect);
 
+
+const generateToken = (userId: string) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET_B!, { expiresIn: '1h' });
+  };
+
+router.get('/check-login', (req, res) => {
+    console.log(req.isAuthenticated());
+    if(!req.isAuthenticated()) {
+        res.redirect('/');
+    }
+    const token = generateToken('asadas'); // Sử dụng JWT để tạo token
+  res.redirect(`http://localhost:3121/callback?token=${token}`);
+
+})
 export default router;
